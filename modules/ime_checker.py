2af8fe720ea5b5fdd757a113d31b737b43072857
Nadya <mikyoc20@gmail.com>
2025-10-25 23:28:17 +0300

    Update ime_checker.py

--- modules/ime_checker.py
+++ modules/ime_checker.py
@@ -21,24 +21,117 @@ class IMEChecker:
             ]
     
     def _create_russian_mappings(self):
-        """Создает словарь для перевода русских терминов в английские"""
-        mappings = {
-            'анафилактический шок': 'Anaphylactic shock',
-            'анафилактическая реакция': 'Anaphylactic reaction', 
-            'анафилаксия': 'Anaphylactic shock',
-            'остановка сердца': 'Cardiac arrest',
-            'инфаркт миокарда': 'Myocardial infarction',
-            'инфаркт': 'Myocardial infarction',
-            'синдром стивенса-джонсона': 'Stevens-Johnson syndrome',
-            'токсический эпидермальный некролиз': 'Toxic epidermal necrolysis',
-            'острая печеночная недостаточность': 'Acute hepatic failure',
-            'острая почечная недостаточность': 'Acute kidney injury',
-            'инсульт': 'Cerebrovascular accident',
-            'сепсис': 'Sepsis',
-            'панкреатит': 'Pancreatitis acute',
-            'суицидальные мысли': 'Suicidal ideation'
-        }
-        return mappings
+    """Создает словарь для перевода русских терминов в английские"""
+    mappings = {
+        # Кардиологические
+        'инфаркт миокарда': 'Myocardial infarction',
+        'ишемия миокарда': 'Myocardial ischemia',
+        'перикардиальный выпот': 'Pericardial effusion',
+        'тромбоз коронарных артерий': 'Coronary artery thrombosis',
+        'окклюзия коронарных артерий': 'Coronary artery occlusion',
+        'артериальный тромбоз': 'Arterial thrombosis',
+        'атриовентрикулярная блокада': 'Atrioventricular block first degree',
+        'желудочковые экстрасистолы': 'Ventricular extrasystoles',
+        'сердцебиение': 'Palpitations',
+        'удлинение интервала qt': 'Prolonged electrocardiogram QT',
+        'артериальная гипертензия': 'Hypertension',
+        'хсн': 'Congestive cardiomyopathy',
+        'суправентрикулярная тахикардия': 'Supraventricular tachycardia',
+        'венозная тромбоэмболия': 'Venous thromboembolism',
+        'артериальная тромбоэмболия': 'Arterial thromboembolism',
+        'тромбоз глубоких вен': 'Deep vein thrombosis',
+        'тэла': 'Pulmonary embolism',
+        
+        # Неврологические и психиатрические
+        'психотическое расстройство': 'Psychotic disorder',
+        'галлюцинации': 'Hallucinations',
+        'гипестезия': 'Hypoaesthesia',
+        'тремор': 'Tremor',
+        'летаргия': 'Lethargy',
+        'периферическая нейропатия': 'Peripheral sensory neuropathy',
+        'головокружение': 'Dizziness',
+        'головная боль': 'Headache',
+        'сонливость': 'Somnolence',
+        'заторможенность': 'Lethargy',
+        'инсульт': 'Cerebrovascular accident',
+        'синкопе': 'Syncope',
+        'гипертензивная энцефалопатия': 'Hypertensive encephalopathy',
+        
+        # Дерматологические
+        'сыпь': 'Rash',
+        'дерматит': 'Dermatitis',
+        'крапивница': 'Urticaria',
+        'зуд': 'Pruritus',
+        'эксфолиативный дерматит': 'Exfoliative dermatitis',
+        'синдром стивенса-джонсона': 'Stevens-Johnson syndrome',
+        'токсический эпидермальный некролиз': 'Toxic epidermal necrolysis',
+        'тяжелые кожные реакции': 'Severe cutaneous adverse reactions',
+        'ладонно-подошвенный синдром': 'Palmar-plantar erythrodysaesthesia syndrome',
+        
+        # Желудочно-кишечные
+        'тошнота': 'Nausea',
+        'рвота': 'Vomiting',
+        'диарея': 'Diarrhoea',
+        'запор': 'Constipation',
+        'боль в животе': 'Abdominal pain',
+        'диспепсия': 'Dyspepsia',
+        'панкреатит': 'Pancreatitis acute',
+        'колит': 'Colitis',
+        'перфорация жкт': 'Gastrointestinal perforation',
+        'непроходимость кишечника': 'Gastrointestinal obstruction',
+        'желудочно-кишечное кровотечение': 'Gastrointestinal haemorrhage',
+        'ректальное кровотечение': 'Rectal haemorrhage',
+        
+        # Печеночные
+        'повышение алт': 'Alanine aminotransferase increased',
+        'повышение аст': 'Aspartate aminotransferase increased',
+        'повышение печеночных ферментов': 'Hepatic enzyme increased',
+        'повышение трансаминаз': 'Transaminases increased',
+        'повышение ггт': 'Gamma-glutamyltransferase increased',
+        'гепатит': 'Hepatitis',
+        'печеночная недостаточность': 'Acute hepatic failure',
+        
+        # Почечные
+        'нарушение функции почек': 'Renal impairment',
+        'почечная недостаточность': 'Renal failure',
+        'острая почечная недостаточность': 'Renal failure acute',
+        'нефрит': 'Nephritis',
+        'протеинурия': 'Proteinuria',
+        'нефротический синдром': 'Nephrotic syndrome',
+        
+        # Гематологические
+        'лейкопения': 'Leukopenia',
+        'нейтропения': 'Neutropenia',
+        'тромбоцитопения': 'Thrombocytopenia',
+        'лимфопения': 'Lymphopenia',
+        'анемия': 'Anaemia',
+        'фебрильная нейтропения': 'Febrile neutropenia',
+        'коагулопатии': 'Coagulopathy',
+        'синдром диссеминированного свертывания': 'Disseminated intravascular coagulation',
+        
+        # Инфекционные
+        'сепсис': 'Sepsis',
+        'пневмония': 'Pneumonia',
+        'инфекции': 'Infection',
+        'некротизирующий фасциит': 'Necrotising fasciitis',
+        
+        # Аллергические и иммунные
+        'анафилактический шок': 'Anaphylactic shock',
+        'анафилактические реакции': 'Anaphylactic reaction',
+        'аллергические реакции': 'Anaphylactic reaction',
+        'реакции гиперчувствительности': 'Hypersensitivity',
+        'инфузионные реакции': 'Infusion related reaction',
+        
+        # Другие серьезные
+        'смерть': 'Death',
+        'летальный': 'Death',
+        'погиб': 'Death',
+        'умер': 'Death',
+        'госпитализирован': 'Hospitalisation',
+        'реанимация': 'Life threatening',
+        'угроза жизни': 'Life threatening'
+    }
+    return mappings
     
     def check_ime_significance(self, text):
         """
